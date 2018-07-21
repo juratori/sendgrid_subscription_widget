e3eacf880b4e5c3a5bf47f1fb3a135a93f54ca56
Sidneyw <sidney.wijngaarde1@ibm.com>
2018-07-21 15:26:55 -0400

    Adds cors

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 const router = require('./server/router');
 
 // App setup
+app.use(cors());
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
